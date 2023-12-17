3cda78695f21c06bcb53c41e3d6aa379405b1a01
JVP15 <jordanvonpordan@gmail.com>
2023-12-16 20:18:48 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,39 @@
+# Decision Mamba
+
+Basically, this is [decision transformers](https://github.com/kzl/decision-transformer), but with the Mamba architecture. 
+Since Mamba has the 'convolutional' mode for parallelizable training, it is a prime candidate for offline RL.
+Since it also has a 'recurrent' mode, it can do fast inference, which would be ideal for online training. 
+
+## Roadmap
+
+1. Train Mamba on offline Mujoco dataset [ ] (working on training code right now)
+2. Evaluate Mamba against base Decision Transformer [ ]
+3. Fine-tune Mamba using an online RL algorithm (probably PPO) [ ]
+
+Contributions welcome
+
+## Installing
+
+Install `requirements.txt` with `pip install -r requirements.txt`. I also ran these commands to installs these packages in Ubuntu:
+```bash
+sudo apt-get update
+sudo apt-get install -y \
+    libgl1-mesa-dev \
+    libgl1-mesa-glx \
+    libglew-dev \
+    libosmesa6-dev \
+    software-properties-common \
+    patchelf \
+    xvfb
+```
+
+Honestly I don't know what any of them do, but they were in the list of packages to install in the [train your first decision transformer](https://github.com/huggingface/blog/blob/main/notebooks/101_train-decision-transformers.ipynb) notebook so I installed them.
+
+### Installing Mujoco
+
+It's super easy to install the newer version of mujoco. Barely an inconvenience. But to access older (v3 and below) versions of the mujoco envs...
+You have to install `mujoco-py` and that is a *pain*. Here's what I found:
+
+1. If you get an error trying to compile `mujoco-py` the first time it runs, try Cython < 3 (which I have in the requirements.txt file). See [this issue](https://github.com/openai/mujoco-py/issues/773) for more info.
+2. If you get an error like `GLIBCXX_3.4.29 not found`, read [this StackOverflow post](https://stackoverflow.com/questions/72205522/glibcxx-3-4-29-not-found). Basically, delete `libstdc++.so.6` (or whatever file is causing the error) from your `anaconda_install_dir/envs/env_name/lib/` folder. 
+If it is coming from a system path, not Anaconda, you probably shouldn't delete it.
---
+++ modelling_mamba.py
@@ -0,0 +1,100 @@
+from functools import partial
+
+import torch.nn as nn
+
+from mamba_ssm.models.mixer_seq_simple import create_block, _init_weights
+
+try:
+    from mamba_ssm.ops.triton.layernorm import RMSNorm, layer_norm_fn, rms_norm_fn
+except ImportError:
+    RMSNorm, layer_norm_fn, rms_norm_fn = None, None, None
+
+
+
+"""needed to modify the mamba model so that it doesn't have an input embedding layer for Decision Mamba"""
+
+class MixerModel(nn.Module):
+    def __init__(
+        self,
+        d_model: int,
+        n_layer: int,
+        ssm_cfg=None,
+        norm_epsilon: float = 1e-5,
+        rms_norm: bool = False,
+        initializer_cfg=None,
+        fused_add_norm=False,
+        residual_in_fp32=False,
+        device=None,
+        dtype=None,
+    ) -> None:
+        factory_kwargs = {"device": device, "dtype": dtype}
+        super().__init__()
+        self.residual_in_fp32 = residual_in_fp32
+
+        # We change the order of residual and layer norm:
+        # Instead of LN -> Attn / MLP -> Add, we do:
+        # Add -> LN -> Attn / MLP / Mixer, returning both the residual branch (output of Add) and
+        # the main branch (output of MLP / Mixer). The model definition is unchanged.
+        # This is for performance reason: we can fuse add + layer_norm.
+        self.fused_add_norm = fused_add_norm
+        if self.fused_add_norm:
+            if layer_norm_fn is None or rms_norm_fn is None:
+                raise ImportError("Failed to import Triton LayerNorm / RMSNorm kernels")
+
+        self.layers = nn.ModuleList(
+            [
+                create_block(
+                    d_model,
+                    ssm_cfg=ssm_cfg,
+                    norm_epsilon=norm_epsilon,
+                    rms_norm=rms_norm,
+                    residual_in_fp32=residual_in_fp32,
+                    fused_add_norm=fused_add_norm,
+                    layer_idx=i,
+                    **factory_kwargs,
+                )
+                for i in range(n_layer)
+            ]
+        )
+
+        self.norm_f = (nn.LayerNorm if not rms_norm else RMSNorm)(
+            d_model, eps=norm_epsilon, **factory_kwargs
+        )
+
+        self.apply(
+            partial(
+                _init_weights,
+                n_layer=n_layer,
+                **(initializer_cfg if initializer_cfg is not None else {}),
+            )
+        )
+
+    def allocate_inference_cache(self, batch_size, max_seqlen, dtype=None, **kwargs):
+        return {
+            i: layer.allocate_inference_cache(batch_size, max_seqlen, dtype=dtype, **kwargs)
+            for i, layer in enumerate(self.layers)
+        }
+
+    def forward(self, input_embeds, inference_params=None):
+        hidden_states = input_embeds
+        residual = None
+        for layer in self.layers:
+            hidden_states, residual = layer(
+                hidden_states, residual, inference_params=inference_params
+            )
+        if not self.fused_add_norm:
+            residual = (hidden_states + residual) if residual is not None else hidden_states
+            hidden_states = self.norm_f(residual.to(dtype=self.norm_f.weight.dtype))
+        else:
+            # Set prenorm=False here since we don't need the residual
+            fused_add_norm_fn = rms_norm_fn if isinstance(self.norm_f, RMSNorm) else layer_norm_fn
+            hidden_states = fused_add_norm_fn(
+                hidden_states,
+                self.norm_f.weight,
+                self.norm_f.bias,
+                eps=self.norm_f.eps,
+                residual=residual,
+                prenorm=False,
+                residual_in_fp32=self.residual_in_fp32,
+            )
+        return hidden_states
